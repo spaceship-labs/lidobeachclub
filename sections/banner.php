627a265807d8496506c91048ad4b2f73e06abd5a
yosassenav <mromerovanessa.soy@gmail.com>
2020-08-14 19:18:04 -0500

    Agregar cambios a maquetacion lido

--- sections/banner.php
+++ sections/banner.php
@@ -3,22 +3,21 @@
 		<div class="cont max-widht1" ng-controller="scrollCTL">
 			
 			<div class="box">
-				<a>desayunos -</a>
+				<a>desayuno -</a>
 				<a>lunch -</a>
 				<a>cócteles -</a>
 				<a>música en vivo -</a>
-				<a>masaje -</a>
-				<a>parrillada</a>
+				<a>masaje</a>
 			</div>
 
-			<img src="/img/logo-lido-blanco.png" alt="">
+			<img src="/img/Nuevo-logo-lido.png" alt="">
 
 			<a ng-click="scrollTo('pointer',$event)" class="scroll-down">
 				<i class="icon-noun_scroll-down_2925967"></i>
 			</a>
 
 			<div class="servicios">
-				
+				<!--
 				<div class="conter wow fadeInLeft" data-wow-delay=".5s">
 					<i class="icon-noun_Music_1932413"></i>
 					<h2>música en vivo</h2>
@@ -29,12 +28,14 @@
 					<h2>parrillada</h2>
 					<p>Sábados y domingos.</p>
 				</div>
-				
+				-->
+				<div></div>
 				<div class="conter wow fadeInRight" data-wow-delay=".5s">
 					<i class="icon-noun_Pet-friendly_2677524"></i>
 					<h2>pet friendly</h2>
 					<p>Aceptamos mascotas con correa en el area de mesas.</p>
 				</div>
+				<div></div>
 
 			</div>
 
